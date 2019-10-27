8767d6c374257e2b9abb44d0da23591d8898c96a
Lewis Nozaki <lhnozaki@gmail.com>
2019-10-26 14:23:36 -1000

    routers created using react-router

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
-import Body from "../components/Body";
-
 import "./App.css";
 
 function App() {
@@ -10,7 +8,6 @@ function App() {
     <>
       <Header />
       <Navbar />
-      <Body />
     </>
   );
 }
---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Messages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <h1>Messages Tab</h1>;
+  }
+}
+
+export default Messages;
