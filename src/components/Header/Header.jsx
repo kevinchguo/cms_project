91c5135a2ed926577ec1c8143d5f80038ca5a58b
Lewis Nozaki <lhnozaki@gmail.com>
2019-10-31 20:14:17 -1000

    made it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styles from "./Header.module.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // import Navbar from "../Navbar";
 
@@ -16,10 +16,9 @@ class Header extends Component {
         <div className={styles.logo}>
           <div>anml</div>
         </div>
+        <div></div>
         <div className={styles.login}>
-          <Link to="/login">
-            <div className={styles.button}>Login</div>
-          </Link>
+          <Link to="/login">Login</Link>
         </div>
       </div>
     );
