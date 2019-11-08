9e83e4116f53de6872b0e2a3c09654ce9a9b8c8d
Justin <jbbenson12@gmail.com>
2019-11-07 21:58:08 -1000

    made action and reducer for search function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
-import Axios from "axios";
+import Axios from 'axios';
 
-export const LOAD_CREATURE = "LOAD_CREATURE";
-export const USER_LOGIN = "USER_LOGIN";
+export const LOAD_CREATURE = 'LOAD_CREATURE';
+export const USER_LOGIN = 'USER_LOGIN';
+export const SEARCH_CREATURE = 'SEARCH_CREATURE';
 
 export const loadCreatureAsync = () => async dispatch => {
-  await fetch("/api/creatures")
+  await fetch('/api/creatures')
     .then(response => {
       return response.json();
     })
@@ -20,15 +21,28 @@ export const loadCreatureAsync = () => async dispatch => {
 };
 
 const userDataTest = {
-  email: "kevin@devleague.com",
-  name: "Kevin",
-  password: "kguo"
+  email: 'kevin@devleague.com',
+  name: 'Kevin',
+  password: 'kguo'
 };
 
 export const userLoginAsync = data => async dispatch => {
-  const res = await Axios.post("/api/users/login", data);
+  const res = await Axios.post('/api/users/login', data);
   dispatch({
     type: USER_LOGIN,
     payload: res
   });
+};
+
+export const searchCreatureFunction = keyword => async dispatch => {
+  await Axios.get('/api/creature/search', keyword)
+    .then(creatures => {
+      dispatch({
+        type: SEARCH_CREATURE,
+        payload: creatures
+      });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
 };
