6b9a14301aff36de9bbcdf929e092c13673c2b1a
Kevin Guo <Kevin@Kevins-Air.home>
2019-11-16 10:34:21 -1000

    set up docker and ec2

---
+++ server/public/precache-manifest.714b1c8feac80afe9280e74bbf749f6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b52bfdfa3c5f0b5327118c713de800c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45118b11e0827d6ae495",
+    "url": "/static/css/main.964ae401.chunk.css"
+  },
+  {
+    "revision": "9db9f5ded4ac23ad8114",
+    "url": "/static/js/2.1f46f6fd.chunk.js"
+  },
+  {
+    "revision": "45118b11e0827d6ae495",
+    "url": "/static/js/main.6da3b6d0.chunk.js"
+  },
+  {
+    "revision": "12486a415f53e1064918",
+    "url": "/static/js/runtime-main.9c83b67a.js"
+  }
+]);
